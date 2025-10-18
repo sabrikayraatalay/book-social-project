b402aca56e772918fbdf415aaf8dacace144ea67
sabrikayraatalay <atalaykayra@hotmail.com>
2025-10-18 22:49:13 +0300

    CI Test 2

--- base-code/src/test/java/com/KayraAtalay/base_code/BaseCodeApplicationStarterTests.java
+++ base-code/src/test/java/com/KayraAtalay/base_code/BaseCodeApplicationStarterTests.java
@@ -2,8 +2,12 @@ package com.KayraAtalay.base_code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = com.KayraAtalay.starter.BaseCodeApplicationStarter.class)
+@TestPropertySource(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class BaseCodeApplicationStarterTests {
 
 	@Test
