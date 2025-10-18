3f53ff76908d72fb631d43451a815845a98bc4ea
sabrikayraatalay <atalaykayra@hotmail.com>
2025-10-18 22:43:05 +0300

    CI test

--- base-code/src/test/java/com/KayraAtalay/base_code/BaseCodeApplicationStarterTests.java
+++ base-code/src/test/java/com/KayraAtalay/base_code/BaseCodeApplicationStarterTests.java
@@ -3,7 +3,7 @@ package com.KayraAtalay.base_code;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.KayraAtalay.starter.BaseCodeApplicationStarter.class)
 class BaseCodeApplicationStarterTests {
 
 	@Test
