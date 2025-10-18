c66dcd39b53e98902f4579ff161b91dfbc624eb8
sabrikayraatalay <atalaykayra@hotmail.com>
2025-10-18 23:04:46 +0300

    CI canceled

--- base-code/src/test/java/com/KayraAtalay/base_code/BaseCodeApplicationStarterTests.java
+++ base-code/src/test/java/com/KayraAtalay/base_code/BaseCodeApplicationStarterTests.java
@@ -2,12 +2,8 @@ package com.KayraAtalay.base_code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = com.KayraAtalay.starter.BaseCodeApplicationStarter.class)
-@TestPropertySource(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-})
+@SpringBootTest
 class BaseCodeApplicationStarterTests {
 
 	@Test
